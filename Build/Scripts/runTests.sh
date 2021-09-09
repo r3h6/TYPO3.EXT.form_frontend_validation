c1464c293af219a32e0e4d0cd15b87e169660533
r3h6 <r3h6@outlook.com>
2021-09-09 23:25:36 +0200

    [TASK] Add github ci action

---
+++ Build/Scripts/runTests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+
+
+# Go to the directory this script is located, so everything else is relative
+# to this dir, no matter from where this script is called.
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd "$THIS_SCRIPT_DIR" || exit 1
+
+# Go to directory that contains the local docker-compose.yml file
+cd ../Docker || exit 1
+
+export PHP=${PHP:-7.4}
+
+
+# Move "7.4" to "php74", the latter is the docker container name
+export DOCKER_PHP_IMAGE=`echo "php${PHP}" | sed -e 's/\.//'`
+export ROOT_DIR=`readlink -f ${PWD}/../../`
+export HOST_UID=`id -u`
+
+docker-compose run "$@"
