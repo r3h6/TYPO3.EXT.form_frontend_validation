9bf62d8a68f31e7d51c756c08b1e4a2a2a61f3d4
r3h6 <r3h6@outlook.com>
2023-06-03 20:16:43 +0200

    Fix wrong constant check

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('form_frontend_validation', 'Configuration/TypoScript/Parsley', 'Form Validation "Parsley"');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('form_frontend_validation', 'Configuration/TypoScript/Parsley/JavaScript', 'Form Validation "Parsley JavaScript"');
--- ext_emconf.php
+++ ext_emconf.php
@@ -19,10 +19,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'beta',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-12.4.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
